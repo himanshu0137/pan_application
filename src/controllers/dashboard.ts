c65d24f0619c41419b1ce03d5d63f40458c3e895
himanshu0137 <himanshu0137.hb@gmail.com>
2018-04-15 09:53:47 +0530

    added forms for vendors and sorted forms

--- src/controllers/dashboard.ts
+++ src/controllers/dashboard.ts
@@ -1,6 +1,7 @@
 import { default as Form, FormModel } from "../models/Form";
 import { default as User, UserModel } from "../models/User";
 import { Request, Response } from "express";
+import { ObjectId } from "bson";
 
 
 const dateTransform = (date: Date) => {
@@ -9,7 +10,11 @@ const dateTransform = (date: Date) => {
     return "";
 };
 export let getDashboard = (req: Request, res: Response) => {
-    Form.find({}).select({"agentId": 1, "nameOnAadhaar": 1, "date": 1}).populate("agentId", "-_id email name").exec((err, doc) => {
+    let findOption = {};
+    if (req.user.role == 2) {
+        findOption = {"agentId": new ObjectId(req.user.id)};
+    }
+    Form.find(findOption).select({"agentId": 1, "nameOnAadhaar": 1, "date": 1}).populate("agentId", "-_id email name").sort("-date").exec((err, doc) => {
         if (err) {
             return res.redirect("/");
         }
