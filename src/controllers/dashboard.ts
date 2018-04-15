95ae621900b8c463cdabe6acf2a593abecc3d612
himanshu0137 <himanshu0137.hb@gmail.com>
2018-04-15 09:41:11 +0530

    transfomed date

--- src/controllers/dashboard.ts
+++ src/controllers/dashboard.ts
@@ -2,17 +2,23 @@ import { default as Form, FormModel } from "../models/Form";
 import { default as User, UserModel } from "../models/User";
 import { Request, Response } from "express";
 
+
+const dateTransform = (date: Date) => {
+    if (date)
+        return date.toDateString();
+    return "";
+};
 export let getDashboard = (req: Request, res: Response) => {
-    Form.find({}).select({ "agentId": 1 }).populate("agentId", "-_id email name").exec((err, doc) => {
+    Form.find({}).select({"agentId": 1, "nameOnAadhaar": 1, "date": 1}).populate("agentId", "-_id email name").exec((err, doc) => {
         if (err) {
             return res.redirect("/");
         }
         else {
             return res.render("dashboard", {
                 title: "DashBoard",
-                forms: doc
+                forms: doc,
+                dateTransform: dateTransform
             });
         }
     });
 };
-
