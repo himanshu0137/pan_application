098254ab8ec32723565a0b1874681c43993790ec
himanshu0137 <himanshu0137.hb@gmail.com>
2018-04-16 22:04:11 +0530

    changed env settings

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -9,7 +9,7 @@ if (fs.existsSync(".env")) {
     logger.debug("Using .env.example file to supply config environment variables");
     dotenv.config({ path: ".env.example" });  // you can delete this after you create your own .env file!
 }
-export const ENVIRONMENT = process.env.NODE_ENV;
+export const ENVIRONMENT = process.env.NODE_ENV || "prod";
 const prod = ENVIRONMENT; // Anything else is treated as 'dev'
 
 export const SESSION_SECRET = process.env["SESSION_SECRET"];
